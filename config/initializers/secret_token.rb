fd1807dd80962c6250f232b1299bca0c435fd34e
Marcin Bratek <github@bratekmarcin.eu>
2011-11-23 17:55:54 +0100

    Create repository for BookWeb RoR application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookweb::Application.config.secret_token = '4996169329537125b64fb9e5818e711b0f4f237c753496aad8d7b228bb7a63a441f6dd94c5d88ec814f0fa3268d774ffd3fabec48765d76de9e6b3c774528d2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bookweb::Application.config.session_store :cookie_store, key: '_bookweb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bookweb::Application.config.session_store :active_record_store
