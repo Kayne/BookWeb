2ed51eea4f7111d0edbff8d78c0bbce6bdfad65c
Marcin Bratek <github@bratekmarcin.eu>
2011-11-23 18:41:19 +0100

    Run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111123170050) do
+ActiveRecord::Schema.define(:version => 20111123173626) do
+
+  create_table "books", :force => true do |t|
+    t.string "title"
+    t.text   "desc"
+    t.string "author"
+    t.string "photo"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
