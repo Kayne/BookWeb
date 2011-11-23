d4361d67075e2414a02b647c371b3e3807ff0051
Marcin Bratek <github@bratekmarcin.eu>
2011-11-23 18:35:53 +0100

    Add authentication for Books Controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
+before_filter :authenticate_user!, :only => [:show]
 
   def index
+  end
+
+  def show
+  # To jest dla testów Devise - tutaj będzie wyświetlana konkretna książka (po ID)
   end
 
 end
