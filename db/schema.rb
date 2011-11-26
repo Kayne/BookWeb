9bb523ac0b52923a321ec0493070287cc092717f
Marcin Bratek <github@bratekmarcin.eu>
2011-11-26 13:08:27 +0100

    Ran db:migrate for CreateBooksAssigmentsTable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111124132255) do
+ActiveRecord::Schema.define(:version => 20111126120142) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(:version => 20111124132255) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+  end
+
+  create_table "booksassigments", :force => true do |t|
+    t.integer "book_id"
+    t.integer "user_id"
   end
 
   create_table "users", :force => true do |t|
