e0e8f1ba0fef0670507f2df32f6addf538acbbe9
Marcin Bratek <github@bratekmarcin.eu>
2011-12-04 23:50:51 +0100

    Added code for FriendlyId gem

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ActiveRecord::Base
   has_many :booksassignments
   has_many :users, :through => :booksassignments
+  # PaperClip gem for photos
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  # FriendlyId
+  extend FriendlyId
+  friendly_id :title, :use => :slugged
 end
