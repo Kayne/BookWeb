54c5947e602999214c6ca6f4e46dc3c706a727b4
Marcin Bratek <github@bratekmarcin.eu>
2011-12-06 00:55:11 +0100

    Added validations for Books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,12 @@
 class Book < ActiveRecord::Base
   has_many :booksassignments
   has_many :users, :through => :booksassignments
+  
+  validates_presence_of :title, :desc, :author
+  validates_uniqueness_of :title
+  validates_length_of :title, :minimum => 4
+  validates_length_of :author, :minimum => 4
+
   # PaperClip gem for photos
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   # FriendlyId
