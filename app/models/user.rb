50c2cf041630958dc641329d96847d133a70536e
Marcin Bratek <github@bratekmarcin.eu>
2011-12-06 18:34:33 +0100

    Modified devise configuration for :login; Added methods in user model for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,40 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
 
+  protected
+
   def self.find_for_authentication(conditions)
     login = conditions.delete(:login)
-      where(conditions).where(["username = :value OR email = :value", { :value => login }]).first
-      end
+    where(conditions).where(["username = :value OR email = :value", { :value => login }]).first
+  end
 
+  def self.find_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
+    case_insensitive_keys.each { |k| attributes[k].try(:downcase!) }
+    
+    attributes = attributes.slice(*required_attributes)
+    attributes.delete_if { |key, value| value.blank? }
+
+    if attributes.size == required_attributes.size
+      if attributes.has_key?(:login)
+        login = attributes.delete(:login)
+	record = find_record(login)
+      else  
+        record = where(attributes).first
+      end  
+    end  
+ 
+    unless record
+      record = new
+      required_attributes.each do |key|
+	value = attributes[key]
+        record.send("#{key}=", value)
+	record.errors.add(key, value.present? ? error : :blank)
+      end  
+    end  
+    record
+  end
+
+  def self.find_record(login)
+    where({:username => login} | { :email => login}).first
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -80,7 +80,7 @@ Devise.setup do |config|
   # config.confirm_within = 2.days
 
   # Defines which key will be used when confirming an account
-  # config.confirmation_keys = [ :email ]
+  config.confirmation_keys = [ :login ]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
@@ -121,7 +121,7 @@ Devise.setup do |config|
   # config.lock_strategy = :failed_attempts
 
   # Defines which key will be used when locking and unlocking an account
-  # config.unlock_keys = [ :email ]
+  config.unlock_keys = [ :login ]
 
   # Defines which strategy will be used to unlock an account.
   # :email = Sends an unlock link to the user email
