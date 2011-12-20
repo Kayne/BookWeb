72a2835fd64d125c40105cba7744b5615078e0da
Marcin Bratek <github@bratekmarcin.eu>
2011-12-20 11:14:39 +0100

    Fixed problem with only one translation; Added translation for ransack gem

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Bookweb
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :pl
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -44,8 +44,5 @@ module Bookweb
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    # Default translation
-    config.i18n.default_locale = :pl
   end
 end
