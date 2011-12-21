69defb92161023e80e67306f8fb05d9d08aeadcc
Marcin Bratek <github@bratekmarcin.eu>
2011-12-21 23:52:35 +0100

    Fixed problem with not nulls; Fixed validations bug in Booksassigment model

--- app/models/booksassigment.rb
+++ app/models/booksassigment.rb
@@ -2,5 +2,5 @@ class Booksassigment < ActiveRecord::Base
   belongs_to :user  # foreign key - user_id
   belongs_to :book  # foriegn key - book_id
 
-  validates_presence_of :book_id, :users_id
+  validates_presence_of :book_id, :user_id
 end
