e4e562d65b38d54645d538b0eb13183496478420
Marcin Bratek <github@bratekmarcin.eu>
2012-01-13 19:21:19 +0100

    Added admin role field for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111225121500) do
+ActiveRecord::Schema.define(:version => 20120113181219) do
 
   create_table "books", :force => true do |t|
     t.string   "title",              :null => false
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(:version => 20111225121500) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -44,11 +44,12 @@ ActiveRecord::Schema.define(:version => 20111225121500) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",                                              :null => false
+    t.string   "username",                                                 :null => false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "admin",                                 :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
