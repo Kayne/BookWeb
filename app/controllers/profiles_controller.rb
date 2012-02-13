091e73df24750ac2721cff860e6d3e73ebf961df
Marcin Bratek <github@bratekmarcin.eu>
2012-02-13 11:39:02 +0100

    Improved online users list

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,9 @@ class ProfilesController < ApplicationController
 
   def index
     @paginate = User.order('id DESC').page(params[:page])
+    id = 0
+    id = current_user.id if user_signed_in?
+    @onlines = User.online_list(id)
   end
 
   def show
